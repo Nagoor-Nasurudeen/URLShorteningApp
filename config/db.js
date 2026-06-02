babf7effafae9c41a718662e617bb59348c8b048
Nagoor Nasurudeen <nagoornasurudeen@gmail.com>
2026-06-03 01:54:04 +0530

    env file added

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "localhost",    // env
-    user: "root",
-    password: "nagoor",
-    database: "urlservicedb",
+    host:process.env.HOST,    // env
+    user:process.env.USER,
+    password: process.env.PASSWORD,
+    database:process.env.DATABASENAME ,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
