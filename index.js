7c2d5244e417f76ab15ac7bf7f0c93e6d069f916
Nagoor Nasurudeen <nagoornasurudeen@gmail.com>
2026-06-03 01:43:29 +0530

    small corrections

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const db = require("./config/db");
 
 app.use(express.json());
 
+//get all URLs
 app.get("/",async (req,res)=>{
     try{
         const [result]=await db.execute("SELECT * FROM urltable");
-        
         res.json(result);
     }catch(err){
         console.error(err);
@@ -20,19 +20,23 @@ app.get("/",async (req,res)=>{
     
 });
 
+app.get("/stats/:shorturl",async (req,res)=>{
+    let shorturl=req.params.shorturl;
+    const [result] =await db.execute("SELECT * FROM urltable WHERE shorturl =?",[shorturl]);
+    res.json(result[0]);
+})
+
 app.post("/shorturls",  async (req,res)=>{
     try{
         let longurl = req.body.longurl;
-        
         //validation and sanitisation
         if(!longurl.startsWith("http://") && !longurl.startsWith("https://")){
             longurl="https://"+longurl;
         }
-
-        
+        longurl= new URL(longurl).href;
 
         let [lastId]= await db.execute("SELECT id FROM urltable order by id  desc limit 1");
-        let lastiUrlId;
+        let lastUrlId;
         //to handle first insert
         if (typeof lastId[0] !== 'undefined'){  
             lastUrlId=lastId[0].id;
@@ -50,11 +54,17 @@ app.post("/shorturls",  async (req,res)=>{
 
         const [result]= await db.execute("INSERT INTO urltable (longurl,shorturl) VALUES (?,?)",[longurl,shorturl]);
         
-        res.json({shorturl});
+        res.json({shorturl:`http://localhost:${PORT}/${shorturl}`});
 
     }catch(err){
+        if (err instanceof TypeError) {
+            console.error(err);
+            return res.status(400).json({message:"The given URL is invalid"})
+        }
+        
         console.error(err);
         res.status(500).json({error:err.message});
+
     }
 })
 
@@ -62,20 +72,22 @@ app.get("/:shorturl",async (req,res)=>{  //return longurl
     try{
         let shorturl=req.params.shorturl;
         const [result]=await db.execute("SELECT id,longurl,visits FROM urltable where shorturl=?",[shorturl]);
-        // console.log(result);
+        //console.log(result);
         //if result is empty then it is  not a valid url
         if(result.length>0){ 
-            const [result2]=await db.execute("UPDATE urltable SET visits = ?+1 WHERE id=?",[result[0].visits,result[0].id]);
+            const [result2]=await db.execute("UPDATE urltable SET visits = visits+1 WHERE id=?",[result[0].id]);
             
             res.redirect(result[0].longurl);
         }else{
-            res.status(404).json({message: `the given shorturl ${shorturl} is invalid. Please provide a valid shorturl`})
+            res.status(404).json({message: `The given shorturl ${shorturl} is invalid. Please provide a valid shorturl`})
         }
     }catch(err){
         console.error(err);
         res.status(500).json({error:err.message});
     }
 });
+
+
 
 app.listen(PORT,()=>{
     console.log("Server started successfully");
